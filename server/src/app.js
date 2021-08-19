3947b9bfc1dbcc9de47354abbc91a4d5747c3540
Kamil <kamil.mieniek@gmail.com>
2021-08-19 19:14:56 +0200

    add deploy script

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const planetsRouter = require('./routes/planets/planets.router');
 const cors = require('cors');
 const app = express();
@@ -6,10 +7,12 @@ const app = express();
 var corsOptions = {
     origin: 'http://localhost:3000'
   }
-
+app.use(express.static(path.join(__dirname,"..","public")))
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(planetsRouter)
-
+app.get("/",(req,res) => {
+  res.sendFile(path.join(__dirname,"..","public","index.html"))
+})
 
 module.exports = app;
