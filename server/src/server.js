ffdb16cbeceda5df909122b749f04b185f70d7f7
KamilMieniek <kamil.mieniek@gmail.com>
2021-08-31 13:16:38 +0200

    Add MongoDb for GitHub Actions

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { mongoConnect } = require('./services/mongo');
 const app = require('./app');
 const { loadLaunchData } = require('./models/launches.model');
-
+const { loadPlanetData } = require('./models/planets.model');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 9000;
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 9000;
 async function startServer() {
   mongoConnect();
   loadLaunchData();
+  loadPlanetData();
   app.listen(PORT, () => {
     console.log(`listening on port ${PORT}...`);
   });
